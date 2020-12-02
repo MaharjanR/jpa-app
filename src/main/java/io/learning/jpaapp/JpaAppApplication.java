375619b79f4d9972767c0485616bb9ea856aaa56
Ritesh Maharjan <rite_maharjan@yahoo.com>
2020-12-02 17:13:38 -0500

    Added commandline runner to add book record

--- src/main/java/io/learning/jpaapp/JpaAppApplication.java
+++ src/main/java/io/learning/jpaapp/JpaAppApplication.java
@@ -1,13 +1,27 @@
 package io.learning.jpaapp;
 
+import io.learning.jpaapp.domain.Book;
+import io.learning.jpaapp.repo.BookRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpaAppApplication {
 
+    BookRepo repo;
+
     public static void main(String[] args) {
         SpringApplication.run(JpaAppApplication.class, args);
     }
 
+    CommandLineRunner runner(){
+        return args -> {
+
+            Book book1 = new Book(1231231l, "Learning React", "Ritesh", "Programming", 500.0);
+
+            repo.save(book1);
+
+        };
+    }
 }
