f72bae5a7a6937762c048bb1f731857e62867700
Ritesh Maharjan <rite_maharjan@yahoo.com>
2020-12-02 17:26:40 -0500

    Did CRUD opertaions of book

--- src/main/java/io/learning/jpaapp/JpaAppApplication.java
+++ src/main/java/io/learning/jpaapp/JpaAppApplication.java
@@ -2,26 +2,39 @@ package io.learning.jpaapp;
 
 import io.learning.jpaapp.domain.Book;
 import io.learning.jpaapp.repo.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaAppApplication {
 
+    @Autowired
     BookRepo repo;
 
     public static void main(String[] args) {
         SpringApplication.run(JpaAppApplication.class, args);
     }
 
+    @Bean
     CommandLineRunner runner(){
         return args -> {
 
+//          create
             Book book1 = new Book(1231231l, "Learning React", "Ritesh", "Programming", 500.0);
+            System.out.println(book1);
+            repo.save(book1);
+            repo.save(new Book(12345678l, "Learning Java", "Safal", "Programming", 5200.0));
+            repo.save(new Book(9876543l, "Learning Spring Boot", "Rag", "Programming", 200.0));
 
+//          Update
+            book1.setTitle("Learning JavaScript");
             repo.save(book1);
 
+//          delete
+            repo.delete(book1);
         };
     }
 }
