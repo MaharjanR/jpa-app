1aca3badaef11d6e62b184b2e2c6a64d652ca368
Ritesh Maharjan <rite_maharjan@yahoo.com>
2020-12-02 17:47:13 -0500

    Created manual sql query

--- src/main/java/io/learning/jpaapp/repo/BookRepo.java
+++ src/main/java/io/learning/jpaapp/repo/BookRepo.java
@@ -2,8 +2,22 @@ package io.learning.jpaapp.repo;
 
 import io.learning.jpaapp.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepo extends JpaRepository<Book, Long> {
+
+//    @Query("select count(a) from  Book a")
+//    long countAll();
+
+
+    @Query(value = "select count(*) from  table_book", nativeQuery = true)
+    long countAll();
+
+    @Query(value = "SELECT * FROM table_book WHERE price < :price", nativeQuery = true)
+    List<Book> getBooksByPriceLessThan(@Param("price")double price);
 }
