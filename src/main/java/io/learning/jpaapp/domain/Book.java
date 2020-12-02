5683b15a86747d067966dac8b867b7164c02cfe4
Ritesh Maharjan <rite_maharjan@yahoo.com>
2020-12-02 17:11:47 -0500

    Created table book

--- src/main/java/io/learning/jpaapp/domain/Book.java
+++ src/main/java/io/learning/jpaapp/domain/Book.java
@@ -3,15 +3,34 @@ package io.learning.jpaapp.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import sun.security.util.Length;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name="Book")
+@Table(name="table_book")
 @NoArgsConstructor
 @Data
 @ToString
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="book_id")
+    private Long id;
+    private Long isbn;
+
+    @Column(length=255)
+    private String title;
+    private String author;
+    private String category;
+    private Double price;
+
+    public Book(Long isbn, String title, String author, String category, Double price) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.price = price;
+    }
 }
