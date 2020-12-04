0d6062ef210f7d197b99ba7b6060494207f9b6cc
Ritesh Maharjan <rite_maharjan@yahoo.com>
2020-12-04 08:01:16 -0500

    changed the uri

--- src/main/java/io/learning/jpaapp/controller/BookController.java
+++ src/main/java/io/learning/jpaapp/controller/BookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/book/")
+@RequestMapping("/api/book")
 public class BookController {
 
     @Autowired
